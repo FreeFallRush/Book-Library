388341892c90695195b8009aaf61be642ca168ff
FreeFallRush <queenofcups.altbot@gmail.com>
2023-02-17 17:57:36 +0200

    Updated main.js
    added myLibrary arr and Book constructor function

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 "use strict";
-
+const booksContainer = document.getElementById("books-container");
 const showForm = document.querySelector(".add-book");
 const hideForm = document.querySelector(".close-btn");
 const formContainer = document.getElementById("newbook-form");
 const submitForm = document.getElementById("submit");
+
+const myLibrary = [];
+
+//Book constructor function
+function Book(title, author, pages, bookstatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.bookstatus = bookstatus;
+  if (bookstatus === "read") this.bookstatus = true;
+  else this.bookstatus = false;
+}
 
 function openForm() {
   formContainer.classList.remove("hidden");
